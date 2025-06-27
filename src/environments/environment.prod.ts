16f02d998e09a4871f19d99afeb5b7f1d353239e
Dario Mangano <contact@sunshine-adventures.net>
2025-06-28 00:43:06 +0200

    chore: remove all secrets and sensitive keys from codebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBQ876_Ci6AWLBV5-nmkqLDKnCI3929v0E",
-    authDomain: "bob-app-9cbfe.firebaseapp.com",
-    projectId: "bob-app-9cbfe",
-    storageBucket: "bob-app-9cbfe.appspot.com",
-    messagingSenderId: "163592997424",
-    appId: "1:163592997424:web:ece12127e2e3f07a66bbf5",
-    measurementId: "G-EMZ3P925JF"
+    apiKey: "REMOVED_FOR_SECURITY",
+    authDomain: "REMOVED_FOR_SECURITY",
+    projectId: "REMOVED_FOR_SECURITY",
+    storageBucket: "REMOVED_FOR_SECURITY",
+    messagingSenderId: "REMOVED_FOR_SECURITY",
+    appId: "REMOVED_FOR_SECURITY",
+    measurementId: "REMOVED_FOR_SECURITY"
   },
-  zohoPassword: 'HzCXsEafd6PK',
-  apiUrl: '',
-  flightRadar24ApiKey: 'your-fr24-api-key',
-  openaiApiKey: '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',
+  zohoPassword: 'REMOVED_FOR_SECURITY',
+  apiUrl: 'http://localhost:3000/api',
+  flightRadar24ApiKey: 'REMOVED_FOR_SECURITY',
+  openaiApiKey: 'REMOVED_FOR_SECURITY',
   
   // Configuration Aviationstack
   aviationstack: {
